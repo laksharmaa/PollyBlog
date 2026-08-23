bcbd604f6cd84a9745ee75d0c3a4aa4f16154af5
laksharmaa <lakshya.00ls05@gmail.com>
2026-08-23 13:54:59 +0530

    feat: implement blog editing functionality and enhance saved blogs page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,1 +1,31 @@
-import {Navigate,Route,Routes} from "react-router-dom";import AppLayout from "../components/layout/AppLayout";import ProtectedRoute from "./ProtectedRoute";import HomePage from "../pages/HomePage";import BlogDetailPage from "../pages/BlogDetailPage";import LoginPage from "../pages/LoginPage";import RegisterPage from "../pages/RegisterPage";import CreateBlogPage from "../pages/CreateBlogPage";import SavedBlogsPage from "../pages/SavedBlogsPage";import TextToSpeechPage from "../pages/TextToSpeechPage";export default function AppRoutes(p){return <Routes><Route element={<AppLayout {...p}/> }><Route path="/" element={<HomePage/>}/><Route path="/public-blog/:blogId" element={<BlogDetailPage/>}/><Route path="/login" element={<LoginPage onLogin={p.onLogin}/>}/><Route path="/register" element={<RegisterPage onLogin={p.onLogin}/>}/><Route element={<ProtectedRoute isAuthenticated={p.isAuthenticated}/>}><Route path="/create-blog" element={<CreateBlogPage/>}/><Route path="/saved-blogs" element={<SavedBlogsPage/>}/><Route path="/text-to-speech" element={<TextToSpeechPage/>}/></Route><Route path="*" element={<Navigate to="/" replace/>}/></Route></Routes>}
+import { Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "../components/layout/AppLayout";
+import ProtectedRoute from "./ProtectedRoute";
+import HomePage from "../pages/HomePage";
+import BlogDetailPage from "../pages/BlogDetailPage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import CreateBlogPage from "../pages/CreateBlogPage";
+import SavedBlogsPage from "../pages/SavedBlogsPage";
+import EditBlogPage from "../pages/EditBlogPage";
+import TextToSpeechPage from "../pages/TextToSpeechPage";
+
+export default function AppRoutes(props) {
+  return (
+    <Routes>
+      <Route element={<AppLayout {...props} />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/public-blog/:blogId" element={<BlogDetailPage />} />
+        <Route path="/login" element={<LoginPage onLogin={props.onLogin} />} />
+        <Route path="/register" element={<RegisterPage onLogin={props.onLogin} />} />
+        <Route element={<ProtectedRoute isAuthenticated={props.isAuthenticated} />}>
+          <Route path="/create-blog" element={<CreateBlogPage />} />
+          <Route path="/saved-blogs" element={<SavedBlogsPage />} />
+          <Route path="/edit-blog/:blogId" element={<EditBlogPage />} />
+          <Route path="/text-to-speech" element={<TextToSpeechPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
+  );
+}
